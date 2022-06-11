b0c82cbe28aafaa016a603030ece64627f545ea0
jhhocs <86144532+jhhocs@users.noreply.github.com>
2022-06-10 18:42:19 -0700

    Added directional indicator

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,27 +5,41 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float moveSpeed = 5f;
+    public float moveSpeed;
 
     public Rigidbody2D rb;
     public Animator animator;
+
+    [SerializeField] Transform DirectionalIndicator;
 
     Vector2 movement;
 
     // Update is called once per frame -- Input
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        MovementInput();
+        Rotate();
     }
 
     // FixedUpdate is called 50 times per second -- Movement
     void FixedUpdate() 
     {
         rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void Rotate() {
+        float angle = Utility.AngleTowardsMouse(DirectionalIndicator.position);
+        DirectionalIndicator.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+    }
+
+    void MovementInput () {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if(movement.x != 0 || movement.y != 0) {
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+        }
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 }
